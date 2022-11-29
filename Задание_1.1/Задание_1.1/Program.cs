c194f40d5a60ed8a5e0e94efd78dc2c8e8090922
ToxicID <119170066+ToxicID@users.noreply.github.com>
2022-11-29 10:29:18 +0300

    Add files via upload

---
+++ Задание_1.1/Задание_1.1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Новожилов_неделя_1_задание_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Первое задание с периметром по двум катетам
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Введие первый катет");
+                    var a = float.Parse(Console.ReadLine());
+                    Console.WriteLine("Введие второй катет");
+                    var b = float.Parse(Console.ReadLine());
+
+                    do
+                    {
+                        if (a > 0 && b > 0)
+                            break;
+                        Console.WriteLine("Ввод данных некорректный. Повторите попытку.");
+
+                        Console.WriteLine("Введие первый катет");
+                        a = float.Parse(Console.ReadLine());
+                        Console.WriteLine("Введие второй катет");
+                        b = float.Parse(Console.ReadLine());
+                    } while (true);
+                    var P = Math.Round(Math.Sqrt(Math.Pow(a, 2) + Math.Pow(b, 2)) + b + a, 4);
+                    Console.WriteLine("Периметр треугольника равен:{0}", P);
+                    break;
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Произошла ошибка, попробуйте ещё раз");
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Задание_1.2/Задание_1.2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Новожилов_неделя_1_задание_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Второе задание с одинаковы ли цифры данного двухзначного числа
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Введите число");
+                    int num = int.Parse(Console.ReadLine());
+                    do
+                    {
+                        if (num >= -99 && num <=-10 || num>=10 && num<=99)
+                        break;
+                        Console.WriteLine("Вы ввели не двухзначное число!!!\nВведите другое число");
+                        num = int.Parse(Console.ReadLine());
+                    } while (true);
+
+
+
+                    bool res = num % 11 == 0 ? true : false;
+                    if (res)
+                        Console.WriteLine("Цифры введённого числа одинаковы");
+                    else
+                        Console.WriteLine("Цифры введённого числа неодинаковы");
+                    break;
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Произошла ошибка, попробуйте ещё раз");
+
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
