98340a6d4a0773d87b932fae2ad05816d2ba6689
ToxicID <119170066+ToxicID@users.noreply.github.com>
2022-12-01 18:34:23 +0300

    Add files via upload

---
+++ Задание_3.1/Задание_3.1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Новожилов_неделя_1_задание_3
+{
+    class Program
+    {
+        static double f(double n)
+        {
+
+            return Math.Sqrt(n) + n;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Вычисление (√6+6)/2 + (√13+13)/2 + (√21+21)/2");
+            double res = (f(6) / 2) + (f(13) / 2) + (f(21) / 2);
+            Console.WriteLine("Результат вычисления=" + Math.Round(res, 5));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Задание_3.2/Задание_3.2/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+
+
+namespace Новожилов_неделя_1_задание_3
+{
+    class Program
+    {
+        //Задание второе
+        static double fun(double x)
+        {
+            double y;
+            if (x < 0)
+                y = -4.0;
+            else if (x >= 1.0)
+                y = 2.0;
+            else
+                y = Math.Pow(x, 2.0) + 3.0 * x + 4.0;
+            return y;
+        }
+        static void Main(string[] args)
+        {
+            double a, b, h;
+            while (true)
+            {
+                Console.Write("Введите от скольки начинается диапозон: ");
+
+
+
+                while (true)
+                {
+                    if (!double.TryParse(Console.ReadLine(), out a))
+                    {
+                        Console.Write("Некорректный ввод. Повторите попытку." +
+                                                    "\nПовторите попытку:");
+                        continue;
+                    }
+                    else
+                        break;
+                }
+
+                Console.Write("Введите до скольки будет идти диапозон: ");
+                while (true)
+                {
+                    if (!double.TryParse(Console.ReadLine(), out b))
+                    {
+                        Console.Write("Некорректный ввод. Повторите попытку." +
+                                                    "\nПовторите попытку:");
+                        continue;
+                    }
+                    else
+                        break;
+                }
+                Console.Write("Введите шаг: ");
+                while (true)
+                {
+                    if (!double.TryParse(Console.ReadLine(), out h))
+                    {
+                        Console.Write("Некорректный ввод. Повторите попытку." +
+                                                    "\nПовторите попытку:");
+                        continue;
+                    }
+                    else if (h <= 0)
+                    {
+                        Console.Write("Некорректный ввод. Повторите попытку." +
+                                                   "\nПовторите попытку:");
+                        continue;
+                    }
+                    else
+                        break;
+                }
+
+                if (a > b)
+                {
+                    Console.WriteLine("Начало диапозона не может быть больше конца диапозона" +
+                            "\nПопробуйте ещё раз");
+
+                    continue;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            Console.WriteLine();
+            for (double i = a; i <= b; i += h)
+            {
+                Console.WriteLine("f({0:f2})={1:f2}", i, fun(i));
+            }
+            Console.ReadLine();
+        }
+    }
+}
+
