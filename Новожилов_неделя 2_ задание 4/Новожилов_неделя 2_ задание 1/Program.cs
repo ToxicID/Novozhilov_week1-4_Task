b285a738f085add57a39e879535bdf15a70195a1
ToxicID <119170066+ToxicID@users.noreply.github.com>
2022-12-07 13:11:28 +0300

    Add files via upload

---
+++ Новожилов_неделя 2_ задание 4/Новожилов_неделя 2_ задание 1/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Новожилов_неделя_2__задание_1
+{
+    class Program
+    {
+        //Первое задание
+
+        //static double rec(double x, int n)
+        //{
+        //    if (n == 0)
+        //        return 1;
+        //    else if (n < 0)
+        //    {
+        //        return 1 / Math.Pow(x, Math.Abs(n));
+        //    }
+        //    //Если n>0 в таком случае происходит вызов этого же метода с уменьшенной на 1 степенью
+        //    return x * rec(x, n - 1);
+        //}
+        //static void Main(string[] args)
+        //{
+        //    double x;
+        //    int n;
+        //    do
+        //    {
+        //        Console.Write("Введите x=");
+        //        if (!double.TryParse(Console.ReadLine(), out x) || x == 0)
+        //        {
+        //            Console.WriteLine("Произошла ошибка попробуй ещё раз");
+        //        }
+        //        else break;
+        //    } while (true);
+
+        //    do
+        //    {
+        //        Console.Write("Число в которое хотите возвести:");
+        //        if (!int.TryParse(Console.ReadLine(), out n))
+        //        {
+        //            Console.WriteLine("Произошла ошибка попробуй ещё раз");
+        //        }
+        //        else break;
+        //    } while (true);
+
+        //    Console.WriteLine($"x = {Math.Round(rec(x, n), 5)}");
+        //    Console.ReadLine();
+        //}
+
+
+        //Второе задание
+        static void rec(int n, int kol)
+        {
+            if (kol > n)
+                return;
+
+
+            for (int i = kol; i > 0; --i)
+            {
+                Console.Write("  " + i);
+            }
+            Console.WriteLine(" ");
+            rec(n, kol + 1);
+
+
+        }
+
+        static void Main()
+        {
+            int n;
+            do
+            {
+                Console.Write("Введите n=");
+                if (!int.TryParse(Console.ReadLine(), out n) || n <= 0)
+                {
+                    Console.WriteLine("Неверный ввод данных. Попробуйте ещё раз");
+                }
+                else break;
+            } while (true);
+
+            rec(n, 1);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Новожилов_неделя 2_ задание 5/Новодилов_неделя 2_ задание 5/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Новожилов_неделя_2__задание_5
+{
+    class Program
+    {
+        static double f(double x)
+        {
+            try
+            {
+                //Данное условие используется для того чтобы под корнем не было 0 или отрицательного числа
+                if (x <= 0.5) throw new Exception();
+                else return Math.Round(x / Math.Sqrt(2 * x - 1),5);
+            }
+            catch
+            {
+                throw;
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Введите начало диапозона:");
+                double start = double.Parse(Console.ReadLine());
+
+                Console.Write("Введите конец диапозона:");
+                double finish = double.Parse(Console.ReadLine());
+
+                Console.Write("Введите шаг:");
+                double h = double.Parse(Console.ReadLine());
+
+                if (start > finish) throw new Exception("Ошибка!!\nНачало диапозона не может быть больше конца диапозона.");
+                else if (h <= 0) throw new Exception("Ошибка!!\nШаг не может быть отрицательным или равным 0.");
+
+                for (double i = start; i <= finish; i += h)
+                    try
+                    {
+                        Console.WriteLine("y({0})={1:f4}", i, f(i));
+                    }
+                    catch
+                    {
+                        Console.WriteLine("y({0})=error", i);
+                    }
+
+            }
+            //Проверка на ввод чисел
+            catch (FormatException)
+            {
+                Console.WriteLine("Неверный ввод данных. Возможно вы пытись ввести запрещённый знак, повторите попытку ещё раз.");
+            }
+            //при возникномении ошибки не связанной с форматом, выводим данные о ошибке указанные в скобках
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+           
+        }
+    }
+}
