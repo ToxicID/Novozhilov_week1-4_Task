f6a07eec2aad0472da697606cb7f4ac5ff9d7b99
ToxicID <119170066+ToxicID@users.noreply.github.com>
2022-12-13 14:55:18 +0300

    Add files via upload

---
+++ Новожилов_неделя 3_задание 7/Новожилов_неделя 3_задание 7/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Text;
+
+namespace Новожилов_неделя3_задание7
+{
+    class Programm
+    {
+        //Первое задание (Удваивает каждое вхождение заданного символа x)
+
+        static void Main()
+        {
+            try
+            {
+                Console.Write("Введите строку:");
+                string? str = Console.ReadLine();
+                if (string.IsNullOrWhiteSpace(str)) throw new Exception();
+                Console.Write("Введите символ, который будет удвоен в введённой строке:");
+                StringBuilder stringBuilder = new StringBuilder(str);
+                string x = Console.ReadLine();
+                if (string.IsNullOrWhiteSpace(x)) throw new Exception();
+
+                stringBuilder.Replace(x, x + x);
+                string res = stringBuilder.ToString();
+                Console.WriteLine();
+                Console.WriteLine($"Изначально введённая строка: {str}\nИзменённая строка: {res}");
+                Console.ReadLine();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Некорретный ввод данных. Повторите попытку ещё раз.");
+            }
+        }
+
+        //Второе задание (Удалить из сообщения все повторяющиеся слова (без учета регистра)
+
+        static void Main()
+        {
+            try
+            {
+                Console.Write("Введите строку:");
+                string str = Console.ReadLine();
+                if (string.IsNullOrWhiteSpace(str)) throw new Exception();
+                string[] slova = str.Split(' ');
+                StringBuilder sb = new StringBuilder();
+
+                for (int i = 0; i < slova.Length; ++i)
+                {
+                    //Если условие истинно в строчке есть одинаковые слова вне зависимости от регистра
+                    //Если равняется -1, тогда просто идёт запись в переменную sb класса StringBilder
+                    //Если не равняется -1 тогда слово, не записывается
+                    if (sb.ToString().ToLower().IndexOf(slova[i].ToLower()) == -1)
+                        sb.Append(slova[i] + " ");
+                }
+                Console.WriteLine();
+                Console.WriteLine($"Изменённая строка: {sb}");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Некорретный ввод данных. Повторите попытку ещё раз.");
+            }
+        }
+    }
+}
---
+++ Новожилов_неделя 3_задание 8/Новожилов_неделя 3_задание 8/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Новожилов_неделя_3_задание_8
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите предложжение:");
+            string text = Console.ReadLine();
+            Regex regex = new Regex(@"\b[уеёыаоэяию][а-я]+\b", RegexOptions.IgnoreCase);
+            // \b -начало на границе слова
+            // RegexOptions.IgnoreCase - игнорирует регистр
+            var result = regex.Replace(text, "");
+            result = Regex.Replace(result, @"\s+", " ");
+            Console.WriteLine(result);
+        }
+    }
+}
