819d6d782436e933dab42357df67a12b3efe8833
ToxicID <119170066+ToxicID@users.noreply.github.com>
2022-12-19 15:46:56 +0300

    Add files via upload

---
+++ Новожилов_неделя 3_задание 12/Новожилов_неделя 3_задание 11/Program.cs
@@ -0,0 +1,449 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Новожилов_неделя_3_задание_11
+{
+class MyNewClass
+{
+    double[][] DoubleArray;
+    int n, m;
+    //Создание массива
+    public MyNewClass(int rows, int cols)
+    {
+        n = rows;
+        m = cols;
+        DoubleArray = new double[n][];
+    }
+    //Заполнение массива
+    public void FillingTheArray()
+    {
+        double num;
+        for (int i = 0; i < DoubleArray.Length; i++)
+        {
+            DoubleArray[i] = new double[m];
+            for (int j = 0; j < m; j++)
+            {
+                Console.Write($"Введите элемент[{i},{j}]: ");
+                do
+                {
+                    if (!double.TryParse(Console.ReadLine(), out num))
+                    {
+                        Console.Write("Произошла ошибка. Попробуйте ещё раз.");
+                        continue;
+                    }
+                    else break;
+                } while (true);
+                DoubleArray[i][j] = num;
+            }
+        }
+    }
+    //Вывод массива на экран
+    public void Print()
+    {
+        for (int i = 0; i < DoubleArray.Length; i++)
+        {
+            for (int j = 0; j < DoubleArray[i].Length; j++)
+            {
+                Console.Write(DoubleArray[i][j] + "\t");
+            }
+            Console.WriteLine();
+        }
+
+    }
+    //Сортировка массива
+    public void Sort()
+    {
+        for (var i = 0; i < DoubleArray.Length; ++i)
+        {
+            Array.Sort(DoubleArray[i]);
+            Array.Reverse(DoubleArray[i]);
+        }
+    }
+    //Количество элементов
+    public int CountElem
+    {
+        get { return n * m; }
+    }
+    //Увеличение на скаляр
+    public double ZoomScalar
+    {
+        set
+        {
+            for (int i = 0; i < DoubleArray.Length; i++)
+            {
+                for (int j = 0; j < DoubleArray[i].Length; j++)
+                {
+                    DoubleArray[i][j] += value;
+                }
+
+            }
+        }
+    }
+    //Двухмерный индексатор
+    public double this[int n, int m]
+    {
+        get { return DoubleArray[n][m]; }
+    }
+    //Перегруженный оператор позволяющий увеличить значение всех элементов на 1
+    public static MyNewClass operator ++(MyNewClass myNewClass)
+    {
+        for (int i = 0; i < myNewClass.DoubleArray.Length; i++)
+        {
+            for (int j = 0; j < myNewClass.DoubleArray[i].Length; j++)
+            {
+                myNewClass.DoubleArray[i][j] += 1;
+            }
+
+        }
+        return myNewClass;
+    }
+    //Перегруженный оператор позволяющий уменьшить значение всех элементов на 1
+    public static MyNewClass operator --(MyNewClass myNewClass)
+    {
+        for (int i = 0; i < myNewClass.DoubleArray.Length; i++)
+        {
+            for (int j = 0; j < myNewClass.DoubleArray[i].Length; j++)
+            {
+                myNewClass.DoubleArray[i][j] -= 1;
+            }
+
+        }
+        return myNewClass;
+    }
+    //констант true и false:
+    //обращение к экземпляру класса дает значение true, если каждая строка массива упорядоченна по возрастанию,
+    ///иначе false.
+    public static bool operator true(MyNewClass myNewClass)
+    {
+        int res = 0;
+        for (int r = 0; r < myNewClass.DoubleArray.Length; r++)
+        {
+            for (int i = 0; i < myNewClass.DoubleArray[r].Length; i++)
+            {
+                for (int j = 0; j < myNewClass.DoubleArray[r].Length - 1; j++)
+                {
+                    if (myNewClass.DoubleArray[r][j] < myNewClass.DoubleArray[r][j + 1])
+                    {
+
+                        res++;
+                    }
+                }
+            }
+        }
+        if (res == 0)
+            return true;
+        else return false;
+    }
+
+    //констант true и false:
+    //обращение к экземпляру класса дает значение true, если каждая строка массива упорядоченна по возрастанию,
+    ///иначе false.
+    public static bool operator false(MyNewClass myNewClass)
+    {
+        int res = 0;
+        for (int r = 0; r < myNewClass.DoubleArray.GetLength(0); r++)
+        {
+            for (int i = 0; i < myNewClass.DoubleArray.GetLength(1); i++)
+            {
+                for (int j = 0; j < myNewClass.DoubleArray.GetLength(1) - 1; j++)
+                {
+                    if (myNewClass.DoubleArray[r][j] < myNewClass.DoubleArray[r][j + 1])
+                    {
+
+                        res++;
+                    }
+                }
+            }
+        }
+        if (res != 0)
+            return false;
+        else return true;
+    }
+    public static MyNewClass operator *(MyNewClass A, MyNewClass B)
+    {
+        int a;
+        int b;
+
+        do
+        {
+            if (A.n == B.n && A.m == B.m)
+            {
+                a = A.n;
+                b = A.m;
+            }
+            else
+            {
+                Console.WriteLine("Массивы не одинакого размера\nПовторите снова");
+                do
+                {
+                    Console.WriteLine("Введите количество строк: ");
+                    if (!int.TryParse(Console.ReadLine(), out a) || a <= 0)
+                    {
+                        Console.Write("Произошла ошибка. Попробуйте ещё раз.\n");
+                        continue;
+                    }
+                    else break;
+                } while (true);
+
+                do
+                {
+                    Console.WriteLine("Введите количество столбцов.");
+                    if (!int.TryParse(Console.ReadLine(), out b) || b <= 0)
+                    {
+                        Console.Write("Произошла ошибка. Попробуйте ещё раз.\n");
+                        continue;
+                    }
+                    else break;
+                } while (true);
+
+                B = new MyNewClass(a, b);
+                continue;
+            }
+            break;
+        } while (true);
+        MyNewClass array = new MyNewClass(a, b);
+        Console.WriteLine("Заполнение:");
+        B = new MyNewClass(a, b);
+        B.FillingTheArray();
+        for (int i = 0; i < array.DoubleArray.Length; i++)
+        {
+            array.DoubleArray[i] = new double[array.m];
+            for (int j = 0; j < array.DoubleArray[i].Length; j++)
+            {
+                    for (int k = 0; k < array.DoubleArray[i].Length; k++)
+                    {
+                        array.DoubleArray[i][j] += A[i, k] * B[k, j];
+                    }
+
+            }
+        }
+        Console.WriteLine("\nМассив:\n");
+        array.Print();
+        return array;
+    }
+    //Преобразования в двухмерный массив (неявное преобразование)
+    public static implicit operator MyNewClass(double[][] mx)
+    {
+        return new MyNewClass(mx);
+
+    }
+    //Преобразования в ступенчатый массив (явное преобразование)
+    public static explicit operator double[][](MyNewClass mx)
+    {
+        return mx.DoubleArray;
+    }
+    //Заполнение двухмерного массива исходя из уже заполненного ступенчатого
+    public MyNewClass(double[][] mx)
+    {
+        DoubleArray = new double[mx.Length][];
+        for (int i = 0; i < mx.Length; ++i)
+        {
+            DoubleArray[i] = new double[mx[i].Length];
+            for (int j = 0; j < mx[i].Length; ++j)
+            {
+                DoubleArray[i][j] = mx[i][j];
+            }
+        }
+    }
+class Program
+{
+static void Main(string[] args)
+{
+int n, m;
+do
+{
+    Console.Write("Введите количество строк: ");
+    if (!int.TryParse(Console.ReadLine(), out n) || n <= 0)
+    {
+        Console.Write("Произошла ошибка. Попробуйте ещё раз.");
+        continue;
+    }
+    else break;
+} while (true);
+
+do
+{
+    Console.Write("Введите количество столбцов: ");
+    if (!int.TryParse(Console.ReadLine(), out m) || m <= 0)
+    {
+        Console.Write("Произошла ошибка. Попробуйте ещё раз.");
+        continue;
+    }
+    else break;
+} while (true);
+
+MyNewClass newClass = new MyNewClass(n, m);
+int res;
+do
+{
+    Console.Write("\nЧто вы хотите сделать?\n" +
+        "1.Заполнить массив\n" +
+        "2.Вывести элементы массива\n" +
+        "3.Отсоритровать элементы каждой строки в порядке убывания\n" +
+        "4.Получить количество элементов в массиве\n" +
+        "5.Увеличить значение всех элементов массива на скаляр\n" +
+        "6.Обратиться к конкретному элементу\n" +
+        "7.Увеличить все элементы массива на 1\n" +
+        "8.Уменьшить все элементы массива на 1\n" +
+        "9.Получить информацию отсорирован массив или нет(константа true)\n" +
+        "10.Получить информацию отсорирован массив или нет(константа false)\n" +
+        "11.Перемножить массивы\n" +
+        "12.Преобразовать массив в двухмерный\n" +
+        "13.Преобразовать массив в ступенчатый\n" +
+        "14.Выход\n");
+
+    Console.Write("_________________________________________\nВаш выбор: ");
+
+    do
+    {
+        if (!int.TryParse(Console.ReadLine(), out res) || res <= 0)
+        {
+            Console.Write("Произошла ошибка. Попробуйте ещё раз.");
+            continue;
+        }
+        else break;
+    } while (true);
+    Console.WriteLine();
+    switch (res)
+    {
+        case 1:
+            newClass.FillingTheArray();
+            break;
+        case 2:
+            Console.WriteLine("Массива:");
+            newClass.Print();
+            break;
+        case 3:
+            newClass.Sort();
+            Console.WriteLine("Массива отсортирован)");
+            break;
+        case 4:
+            Console.WriteLine($"Количество элементов в массиве: {newClass.CountElem}");
+            break;
+        case 5:
+            Console.Write("Введите скаляр: ");
+            double scalar;
+            do
+            {
+                if (!double.TryParse(Console.ReadLine(), out scalar) || scalar <= 0)
+                {
+                    Console.Write("Произошла ошибка. Попробуйте ещё раз.");
+                    continue;
+                }
+                else break;
+            } while (true);
+            newClass.ZoomScalar = scalar;
+            Console.WriteLine("Все элементы увеличины на скаляр");
+            break;
+        case 6:
+            int row, col;
+            do
+            {
+                do
+                {
+                    Console.Write("Введите номер строки: ");
+                    if (!int.TryParse(Console.ReadLine(), out row) || row <= -1)
+                    {
+                        Console.WriteLine("Ошибка, попробуйте ещё раз");
+                        continue;
+                    }
+                    break;
+                } while (true);
+
+                do
+                {
+                    Console.Write("Введите номер столбца: ");
+                    if (!int.TryParse(Console.ReadLine(), out col) || col <= -1)
+                    {
+                        Console.Write("Ошибка, попробуйте ещё раз");
+                        continue;
+                    }
+                    break;
+                } while (true);
+
+
+
+                break;
+            } while (true);
+            try
+            {
+                Console.WriteLine($"Элемент под индексом [{row},{col}]: " + newClass[row, col]);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Ошибка.Повторите ввод заново");
+                continue;
+            }
+            break;
+        case 7:
+            Console.WriteLine("Все элементы увеличены на 1\n");
+            newClass++;
+            Console.WriteLine("Массив:");
+            newClass.Print();
+            break;
+        case 8:
+            Console.WriteLine("Все элементы уменьшены на 1\n");
+            newClass--;
+            Console.WriteLine("Массив:");
+            newClass.Print();
+            break;
+        case 9:
+        case 10:
+
+            if (newClass) Console.WriteLine("Строки массива упорядочены по возростаню.");
+            else
+                Console.WriteLine("Строки массива не упорядочены по возростанию.");
+
+            break;
+
+        case 11:
+            Console.Write("Перемножить массивы\n");
+            int a, b;
+            do
+            {
+                Console.WriteLine("Введите количество строк: ");
+                if (!int.TryParse(Console.ReadLine(), out a) || a <= 0)
+                {
+                    Console.Write("Произошла ошибка. Попробуйте ещё раз.\n");
+                    continue;
+                }
+                else break;
+            } while (true);
+
+            do
+            {
+                Console.WriteLine("Введите количество столбцов.");
+                if (!int.TryParse(Console.ReadLine(), out b) || b <= 0)
+                {
+                    Console.Write("Произошла ошибка. Попробуйте ещё раз.\n");
+                    continue;
+                }
+                else break;
+            } while (true);
+
+            MyNewClass myNewClass = new MyNewClass(a, b);
+
+            newClass *= myNewClass;
+
+            break;
+
+        case 12:
+            MyNewClass array = newClass;
+            Console.WriteLine("Массив преобразованный в двухмерный");
+            array.Print();
+            break;
+
+        case 13:
+            MyNewClass array1 = (double[][])newClass;
+            Console.WriteLine("Массив преобразованный в ступенчатый");
+            array1.Print();
+            break;
+    }
+} while (res != 14);
+Console.WriteLine("Было приятно с вами работать. Удачи, возвращайся.");
+
+    }
+}
+}
+
+}
---
+++ Новожилов_неделя 4_задание 13/Новожилов_неделя 4_задание 13/Program.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace абстракт7
+{
+    //Абстрактный класс
+    abstract class Tovar
+    {
+        abstract public void PRINT();
+        abstract public bool TYPE(string temp);
+    }
+    //класс игрушка
+    class Toy : Tovar
+    {
+        string type = "Toy";
+        string name;
+        double price;
+        string madein;
+        string material;
+        int age;
+        public Toy(string name, double price, string madein, string material, int age)
+        {
+            this.name = name;
+            this.price = price;
+            this.madein = madein;
+            this.material = material;
+            this.age = age;
+        }
+        public override void PRINT()
+        {
+            Console.WriteLine("\nТип товара:" + type + "\nНазвание:" + name + "\nЦена:" + price + "\nПроизводитель:" + madein + "\nМатериал:" + material + "\nВозраст:" + age);
+        }
+        public override bool TYPE(string temp)
+        {
+            return temp.Contains(type);
+        }
+    }
+    //Класс книга
+    class Book : Tovar
+    {
+        string type = "Book";
+        string name;
+        string author;
+        double price;
+        string madein;
+        int age;
+        public Book(string name, string author, double price, string madein, int age)
+        {
+            this.name = name;
+            this.author = author;
+            this.price = price;
+            this.madein = madein;
+            this.age = age;
+        }
+        public override void PRINT()
+        {
+            Console.WriteLine("\nТип товара:" + type + "\nНазвание:" + name + "\nАвтор:" + author + "\nЦена:" + price + "\nИздательство:" + madein + "\nВозраст:" + age);
+        }
+        public override bool TYPE(string temp)
+        {
+            return temp.Contains(type);
+        }
+    }
+    //Класс спорт-инвертарь
+    class Sport : Tovar
+    {
+        string type = "Sport";
+        string name;
+        double price;
+        string madein;
+        int age;
+        public Sport(string name, double price, string madein, int age)
+        {
+            this.name = name;
+            this.price = price;
+            this.madein = madein;
+            this.age = age;
+        }
+        public override void PRINT()
+        {
+            Console.WriteLine("\nТип товара:" + type + "\nНазвание:" + name + "\nЦена:" + price + "\nПроизводитель:" + madein + "\nВозраст:" + age);
+        }
+        public override bool TYPE(string temp)
+        {
+            return temp.Contains(type);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+            string? temp;
+
+            //Запись из файла
+            string[] str1;
+            int countLines = File.ReadAllLines("text.txt").Length;
+
+            Tovar[] newObj = new Tovar[countLines];
+            using (StreamReader sr = new StreamReader("text.txt"))
+            {
+                int i = 0;
+                while (sr.Peek() > -1)
+                {
+                    
+                        str1 = sr.ReadLine().Split(':');
+
+                        if (str1[0] == "Toy")
+                            newObj[i] = new Toy(str1[1], double.Parse(str1[2]), str1[3], str1[4], int.Parse(str1[5]));
+                        else if (str1[0] == "Book")
+                            newObj[i] = new Book(str1[1], str1[2], double.Parse(str1[3]), str1[4], int.Parse(str1[5]));
+                        else if (str1[0] == "Sport")
+                            newObj[i] = new Sport(str1[1], double.Parse(str1[2]), str1[3], int.Parse(str1[4]));
+
+                        i++;
+                    
+                    
+                }
+            }
+
+            Console.Write("Из файла");
+            for (int i = 0; i < countLines; i++)
+                newObj[i].PRINT();
+
+            do
+            {
+                Console.Write("Введите тип товаров, которые нужно найти (Toy,Book,Sport), чтобы выйти stop: ");
+                count = 0;
+                temp = Console.ReadLine();
+                for (int i = 0; i < countLines; i++)
+                    if (newObj[i].TYPE(temp))
+                    {
+                        newObj[i].PRINT();
+                        count++;
+                    }
+
+                if (temp == "stop")
+                    break;
+                if (count == 0)
+                    Console.WriteLine("Такого типа товаров в базе нет");
+
+            } while (true);
+
+
+        }
+    }
+}
